e5be3a13f7f2b1057e87d7f2526fe305eb5def01
elisagc <elisagcubero@gmail.com>
2019-11-20 22:07:08 +0100

    creado efecto de giro con flex

--- src/App.tsx
+++ src/App.tsx
@@ -14,9 +14,11 @@ const App: React.FC = () => {
             <div className="card__side card__side--back">Back</div>
           </div>
         </div>
-
         <div className="col-1-of-2">
-          <div className="card">card text</div>
+          <div className="card">
+            <div className="card__side card__side--front">Front</div>
+            <div className="card__side card__side--back">Back</div>
+          </div>
         </div>
       </div>
     </section>
