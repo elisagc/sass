92f24e384e08b5426a75fc93d05cc1c631e14757
elisagc <elisagcubero@gmail.com>
2019-11-20 20:58:25 +0100

    añadido front y back

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 
-
-
 const App: React.FC = () => {
   return (
-    <div data-testid="main">
-     hello
-    </div>
+    <section className="section-tours">
+      <div>
+        <h2>Most popular tours</h2>
+      </div>
+
+      <div className="row">
+        <div className="col-1-of-2">
+          <div className="card">
+            <div className="card__side card__side--front">Front</div>
+            <div className="card__side card__side--back">Back</div>
+          </div>
+        </div>
+
+        <div className="col-1-of-2">
+          <div className="card">card text</div>
+        </div>
+      </div>
+    </section>
   );
 };
 
