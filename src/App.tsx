32a47d19e3bdf0123312112c1d5551a210236a24
elisagc <elisagcubero@gmail.com>
2019-11-21 22:06:26 +0100

    integrado

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import CartaGira from "./CartaGira";
+import Form1 from "./Forms/Form1";
 import Todo from "./Forms/Todo";
+import ImagenCentral from "./imagenCentral";
+import MoveText from "./MoveText";
+import Skew from "./SkewText";
+import TarjetaIcono from "./TarjetasIcon";
 
 const App: React.FC = () => {
   return (
     <div className="section">
+      <ImagenCentral />
+      <CartaGira />
+      <MoveText />
+      <Skew />
+      <TarjetaIcono />
+      <Form1 />
       <Todo />
     </div>
   );
