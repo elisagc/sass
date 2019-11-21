e042e0938a474833332690ba7493ab7e02a67c2a
elisagc <elisagcubero@gmail.com>
2019-11-21 08:35:51 +0100

    creada imagen de fondo con gradient y clip path

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React from "react";
-import CartaGira from "./CartaGira";
 import ImagenCentral from "./imagenCentral";
-import MoveText from "./MoveText";
 
 const App: React.FC = () => {
   return (
     <div className="section">
       <ImagenCentral />
-      <MoveText />
-      <CartaGira />
     </div>
   );
 };
--- src/imagenCentral.tsx
+++ src/imagenCentral.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ImagenCentral: React.FC = () => {
-  return <div className="header">imagen</div>;
+  return <div className="box-imagen">imagen</div>;
 };
 
 export default ImagenCentral;
