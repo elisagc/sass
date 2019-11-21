0401cff0974f7a6f83f641d0855b79910a0d160a
elisagc <elisagcubero@gmail.com>
2019-11-21 09:18:00 +0100

    skew en title con transparencia

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import CartaGira from "./CartaGira";
 import ImagenCentral from "./imagenCentral";
 import MoveText from "./MoveText";
+import Skew from "./SkewText";
 
 const App: React.FC = () => {
   return (
     <div className="section">
       <ImagenCentral />
+      <Skew />
       <MoveText />
       <CartaGira />
     </div>
---
+++ src/SkewText.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Skew: React.FC = () => {
+  return (
+    <div className="skew">
+      <h1 className="skew-text">is like a sweet panda</h1>
+    </div>
+  );
+};
+
+export default Skew;
