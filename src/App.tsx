8d61a8022a676d8e3dfdba1b84e46312a7346bcb
elisagc <elisagcubero@gmail.com>
2019-11-21 08:37:27 +0100

    integrado con imagen

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import CartaGira from "./CartaGira";
 import ImagenCentral from "./imagenCentral";
+import MoveText from "./MoveText";
 
 const App: React.FC = () => {
   return (
     <div className="section">
       <ImagenCentral />
+      <MoveText />
+      <CartaGira />
     </div>
   );
 };
